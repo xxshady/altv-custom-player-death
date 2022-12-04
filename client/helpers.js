726daa68428506d4161fc56b84922243c18fd393
xshady <fastik036@gmail.com>
2022-12-04 14:45:58 +0300

    remove debug log

--- client/helpers.js
+++ client/helpers.js
@@ -2,7 +2,6 @@ export function shuffleArray(array) {
   let index = array.length
   while (index !== 0) {
     const randomIndex = Math.floor(Math.random() * index)
-    console.log(randomIndex)
     --index;
     [array[index], array[randomIndex]] = [
       array[randomIndex], array[index]]
